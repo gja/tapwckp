520e9819be914a3e4e552d13ad6db24a4058e0a6
Tejas Dinkar <tejas@gja.in>
2011-05-05 00:38:39 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TakeAPhotoWithCKPonnappa::Application.config.secret_token = '916d7a2a14ab24e3c07a352024a97ffdbfacb848cae978106a6b145e27085f03277349468b29134834451c0c47462774fd6a7473824164111b6902fff278dcb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TakeAPhotoWithCKPonnappa::Application.config.session_store :cookie_store, :key => '_take_a_photo_with_c_k_ponnappa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TakeAPhotoWithCKPonnappa::Application.config.session_store :active_record_store
